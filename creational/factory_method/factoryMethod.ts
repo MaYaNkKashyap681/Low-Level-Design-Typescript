0cb479528158b4243de69f11a3b2d0c2dac86757
MaYaNkKashyap681 <mayankkashyap7970@gmail.com>
2023-08-28 19:38:06 +0530

    Implementing Factort Method

---
+++ creational/factory_method/factoryMethod.ts
@@ -0,0 +1,43 @@
+
+// Interface of the Shape 
+interface Shape {
+    draw(param?: any): void;
+}
+
+// Concrete Classes
+class Square implements Shape {
+    draw() {
+        console.log("This is a Square")
+    }
+}
+
+class Circle implements Shape {
+    draw() {
+        console.log("This is a Circle")
+    }
+}
+
+//ShapeFactory Class retunrs Shape type of Object
+class ShapeFactory {
+    public getShape(shapeType: string){
+        switch(shapeType) {
+            case 'Circle': return new Circle()
+            case 'Square': return new Square()
+            default: return null;
+        }
+    }
+}
+
+//main function
+function main() {
+  var shapeFactoryObj = new ShapeFactory();
+  var circle = shapeFactoryObj.getShape("Circle");
+  var square = shapeFactoryObj.getShape("Square");
+  circle.draw(); // This is a Circle
+  square.draw(); // This is a Square
+}
+
+main();
+
+
+
